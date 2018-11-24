13e601dc6c00dc4a71d25432cb0ec68ca1b15102
Grasseh <steve@grasseh.com>
2018-11-23 20:10:00 -0500

    Working prototype with no data retention

--- bot.js
+++ bot.js
@@ -1,6 +1,7 @@
-var Discord = require('discord.io');
-var logger = require('winston');
-var auth = require('./auth.json');
+const Discord = require('discord.js');
+const logger = require('winston');
+const auth = require('./auth.json');
+const prototypeGag = require('./gags/prototypeGag');
 // Configure logger settings
 logger.remove(logger.transports.Console);
 logger.add(new logger.transports.Console, {
@@ -8,24 +9,27 @@ logger.add(new logger.transports.Console, {
 });
 logger.level = 'debug';
 // Initialize Discord Bot
-var bot = new Discord.Client({
-    token: auth.token,
-    autorun: true
-});
+const bot = new Discord.Client();
+
+bot.login(auth.token);
 bot.on('ready', function (_evt) {
     logger.info('Connected');
     logger.info('Logged in as: ');
-    logger.info(bot.username + ' - (' + bot.id + ')');
+    logger.info(bot.user.tag);
 });
 let gaggedList = [];
-bot.on('message', function (user, userID, channelID, message, _evt) {
+bot.on('message', msg => {
     // Our bot needs to know if it will execute a command
     // It will listen for messages that will start with `!`
+    let message = msg.content;
+    let user = msg.author;
+    let channel = msg.channel;
+    let guild = msg.guild;
     logger.info(message);
-    logger.info(userID);
-    logger.info(channelID);
-    logger.info(message);
-    logger.info(_evt);
+    logger.info(user.username);
+    logger.info(user.id);
+    logger.info(channel.id);
+    logger.info(guild.id);
     if (message.substring(0, 1) === '!') {
         var args = message.substring(1).split(' ');
         var cmd = args[0];
@@ -34,60 +38,45 @@ bot.on('message', function (user, userID, channelID, message, _evt) {
         switch(cmd) {
         // !ping
         case 'gag':
-            gagSomeone(cmd, gaggedList, bot, message, channelID);
+            gagSomeone(gaggedList, channel, args);
             break;
         case 'ungag':
-            ungagSomeone(cmd, gaggedList, bot, message, channelID);
+            ungagSomeone(gaggedList, channel, args);
             break;
         case 'gaglist':
-            listGaggedUsers(gaggedList, bot, channelID);
+            listGaggedUsers(gaggedList, channel);
             break;
         // Just add any case commands if you want to..
         }
         return;
     }
-    if(userIsGagged(user, gaggedList)){
-        gagMessage(user, message, bot, channelID);
+    if(userIsGagged(user.username, gaggedList)){
+        gagMessage(user.username, msg, message, channel);
     }
 });
 
-function gagSomeone(cmd, gaggedList, bot, message, channelID){
-    let gaggedUser = message.substring(5);
+function gagSomeone(gaggedList, channel, args){
+    let gaggedUser = args[0];
     if(userIsGagged(gaggedUser, gaggedList)){
-        bot.sendMessage({
-            to: channelID,
-            message: `User ${gaggedUser} is already gagged!`
-        });
+        channel.send(`User ${gaggedUser} is already gagged!`);
         return;
     }
     gaggedList.push(gaggedUser);
-    bot.sendMessage({
-        to: channelID,
-        message: `User gagged: ${gaggedUser} !`
-    });
+    channel.send(`User gagged: ${gaggedUser} !`);
 }
 
-function ungagSomeone(cmd, gaggedList, bot, message, channelID){
-    let ungaggedUser = message.substring(7);
+function ungagSomeone(gaggedList, channel, args){
+    let ungaggedUser = args[0];
     if(!userIsGagged(ungaggedUser, gaggedList)){
-        bot.sendMessage({
-            to: channelID,
-            message: `User ${ungaggedUser} is not currently gagged!`
-        });
+        channel.send(`User ${ungaggedUser} is not currently gagged!`);
         return;
     }
     gaggedList.splice(gaggedList.indexOf(ungaggedUser), 1 );
-    bot.sendMessage({
-        to: channelID,
-        message: `User ungagged: ${ungaggedUser} !`
-    });
+    channel.send(`User ungagged: ${ungaggedUser} !`);
 }
 
-function listGaggedUsers(gaggedList, bot, channelID){
-    bot.sendMessage({
-        to: channelID,
-        message: `List of gagged users: ${gaggedList} !`
-    });
+function listGaggedUsers(gaggedList, channel){
+    channel.send(`List of gagged users: ${gaggedList} !`);
 }
 
 function userIsGagged(gaggedUser, gaggedList){
@@ -95,17 +84,16 @@ function userIsGagged(gaggedUser, gaggedList){
     return index !== -1;
 }
 
-function gagMessage(user, message, bot, channelID){
-    //message.delete().catch(_err => {
-    //console.log(`${message.guild.name} ${message.guild.id} Missing perms`);
-    //});
-    let gaggedMessage = convertToGagSpeak(message);
-    bot.sendMessage({
-        to: channelID,
-        message: `Message from ${user}: ${gaggedMessage}.`
+function gagMessage(user, msg, message, channel){
+    msg.delete().catch(_err => {
+        console.log(`${msg.guild.name} ${msg.guild.id} GAGGED`);
     });
+    let gaggedMessage = convertToGagType(message, user);
+    channel.send(`Message from ${user}:\n ${gaggedMessage}.`);
+    //If Discord ever allows to edit a user's message
+    //msg.edit('Message from ${user}: ${gaggedMessage}.`);
 }
 
-function convertToGagSpeak(_message){
-    return 'HMPFF';
+function convertToGagType(message, _user){
+    return prototypeGag(message);
 }
